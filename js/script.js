a739d13274c7f9adda8c338feeb6658fc946c401
Lip-7 <zhighofficial@gmail.com>
2023-03-14 16:13:57 +0100

    minor fixes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 /* declare our variables */
-const imglist = ['src="./img/01.webp" alt="spiderman', 'src="./img/02.webp" alt="racket and clank"', 'src="./img/03.webp" alt="fortnite"', 'src="./img/04.webp" alt="stray"', 'src="./img/05.webp" alt="avengers"']
-const mainImg = '<img class="w-100 h-100 object-fit-cover" >'
-const thumbImg = '<div class="single-img rounded-4 w-100"><img class="w-100 h-100" ></div>'
+const imglist = ['src="./img/01.webp" alt="spiderman', 'src="./img/02.webp" alt="racket and clank"', 'src="./img/03.webp" alt="fortnite"', 'src="./img/04.webp" alt="stray"', 'src="./img/05.webp" alt="avengers"'];
+let mainImgs = '';
+let thumbImgs = '';
+const mainImgWrap = document.getElementById('mainImgWrap')
 
-function generateImgs (imgList, mainImg, thumbImg){
-    for 
+
+for (i = 0; i < imglist.length; i++){
+    let newMainImg =`<img class="w-100 h-100 object-fit-cover" ${imglist[i]}>`;
+    let newThumbImg = `<div class="single-img rounded-4 w-100"><img class="w-100 h-100" ${imglist[i]}></div>`;
+    mainImgs += newMainImg;
+    thumbImgs += newThumbImg;
+    console.log(newMainImg)
 }
+mainImgWrap.innerHTML += mainImgs
