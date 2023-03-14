9ae84f008af6c3bb84d5ce87cb18eab65ca5a471
Lip-7 <zhighofficial@gmail.com>
2023-03-14 15:55:31 +0100

    starting js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* declare our variables */
+const imglist = ['src="./img/01.webp" alt="spiderman', 'src="./img/02.webp" alt="racket and clank"', 'src="./img/03.webp" alt="fortnite"', 'src="./img/04.webp" alt="stray"', 'src="./img/05.webp" alt="avengers"']
+const mainImg = '<img class="w-100 h-100 object-fit-cover" >'
+const thumbImg = '<div class="single-img rounded-4 w-100"><img class="w-100 h-100" ></div>'
+
+function generateImgs (imgList, mainImg, thumbImg){
+    for 
+}
